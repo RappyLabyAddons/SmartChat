192d3a012ab055b4aff92ddccdba79a82e97d547
RappyTV <rappytv@rappytv.com>
2025-05-18 10:20:09 +0200

    Improve descriptions

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,26 +9,25 @@ group = "de.jardateien.smartchat"
 version = providers.environmentVariable("VERSION").getOrElse("1.0.0")
 
 labyMod {
-    defaultPackageName = "de.jardateien.smartchat" //change this to your main package name (used by all modules)
-
-    minecraft {
-        registerVersion(versions.toTypedArray()) {
-            runs {
-                getByName("client") {
-                    // When the property is set to true, you can log in with a Minecraft account
-                    // devLogin = true
-                }
-            }
-        }
-    }
+    defaultPackageName = "de.jardateien.smartchat"
 
     addonInfo {
         namespace = "smartchat"
         displayName = "SmartChat"
         author = "JarDateien"
-        description = "Lets you send information into chat using 'keys'"
+        description = "Lets you send information into chat using placeholders"
         minecraftVersion = "*"
         version = rootProject.version.toString()
+    }
+
+    minecraft {
+        registerVersion(versions.toTypedArray()) {
+            runs {
+                getByName("client") {
+                    devLogin = true
+                }
+            }
+        }
     }
 }
 
