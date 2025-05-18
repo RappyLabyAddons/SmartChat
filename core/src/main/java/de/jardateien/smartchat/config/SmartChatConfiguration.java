08ea9e30554d11c2f8b91acbe84988edef8a5cb5
RappyTV <rappytv@rappytv.com>
2025-05-18 10:33:44 +0200

    Improve ping sound config section * Rename enabledPing option * Make sub settings depend on pingSound option * Increase min sound volume

--- core/src/main/java/de/jardateien/smartchat/config/SmartChatConfiguration.java
+++ core/src/main/java/de/jardateien/smartchat/config/SmartChatConfiguration.java
@@ -6,6 +6,7 @@ import net.labymod.api.client.gui.screen.widget.widgets.input.SwitchWidget.Switc
 import net.labymod.api.client.gui.screen.widget.widgets.input.TextFieldWidget.TextFieldSetting;
 import net.labymod.api.configuration.loader.annotation.ConfigName;
 import net.labymod.api.configuration.loader.property.ConfigProperty;
+import net.labymod.api.configuration.settings.annotation.SettingRequires;
 import net.labymod.api.configuration.settings.annotation.SettingSection;
 
 @ConfigName("settings")
@@ -22,9 +23,11 @@ public class SmartChatConfiguration extends AddonConfig {
 
   @SettingSection(value = "chatNotification", center = true)
   @SwitchSetting
-  private final ConfigProperty<Boolean> enabledPing = new ConfigProperty<>(true);
-  @SliderSetting(min = 0F, max = 2F, steps = 0.1F)
+  private final ConfigProperty<Boolean> pingSound = new ConfigProperty<>(true);
+  @SettingRequires("pingSound")
+  @SliderSetting(min = 0.1F, max = 2F, steps = 0.1F)
   private final ConfigProperty<Float> volume = new ConfigProperty<>(1F);
+  @SettingRequires("pingSound")
   @SliderSetting(min = 0F, max = 2F, steps = 0.1F)
   private final ConfigProperty<Float> pitch = new ConfigProperty<>(1.2F);
 
@@ -41,7 +44,7 @@ public class SmartChatConfiguration extends AddonConfig {
   }
 
   public ConfigProperty<Boolean> enabledPing() {
-    return this.enabledPing;
+    return this.pingSound;
   }
   public ConfigProperty<Float> getVolume() {
     return this.volume;
